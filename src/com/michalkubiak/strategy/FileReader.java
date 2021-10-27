3cda6feb0bbf18a18cca347af421e3eed5a60f3f
Michal <michal.p.kubiak@students.ittralee.ie>
2021-10-27 18:54:45 +0100

    File reader class, violates SRP principle

--- src/com/michalkubiak/strategy/FileReader.java
+++ src/com/michalkubiak/strategy/FileReader.java
@@ -2,4 +2,21 @@ package com.michalkubiak.strategy;
 
 public class FileReader {
 
+    private String compression;
+    private String filter;
+
+    public FileReader(String compression, String filter) {
+        this.compression = compression;
+        this.filter = filter;
+    }
+
+    public void storeFile(String file){
+        if (compression == "jpeg")
+            System.out.println("Compression with JPEG");   // usually this be an algorithm
+        else if (compression == "png")
+            System.out.println("Compresion with PNG");
+
+        if(filter == "BlackAndWhite")
+            System.out.println("Apply Black And White filter"); // And so on
+    }
 }
