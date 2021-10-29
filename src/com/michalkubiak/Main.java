1a95602e88677f7ffd0e835504220475097a2083
Michal <michal.p.kubiak@students.ittralee.ie>
2021-10-29 14:36:07 +0100

    Main, New fileReader file with filetr and compression, and file run

--- src/com/michalkubiak/Main.java
+++ src/com/michalkubiak/Main.java
@@ -1,8 +1,25 @@
 package com.michalkubiak;
 
+import com.michalkubiak.strategy.BlackAndWhite;
+import com.michalkubiak.strategy.FileReader;
+import com.michalkubiak.strategy.JPEGCompression;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+        // We create a new fileReader which does JPEG compression ona file
+        // In this case an image lets say
+        // And will apply JPEG Compression and a Black and White filter!
+
+        var fileReader = new FileReader(
+                new JPEGCompression(),
+                new BlackAndWhite()
+                );
+
+        // Apply the store file method
+        fileReader.storeFile("Family Photo");
+
+
+    };
 }
