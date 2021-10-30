96585acfe5a2c44b96b762a6c341f7c26bf723d6
Michal <michal.p.kubiak@students.ittralee.ie>
2021-10-30 16:51:20 +0100

    Main Class implementation and button call

--- src/com/michalkubiak/Main.java
+++ src/com/michalkubiak/Main.java
@@ -1,11 +1,21 @@
 package com.michalkubiak;
 
+import com.michalkubiak.command.AddUserCommand;
+import com.michalkubiak.command.Button;
+import com.michalkubiak.command.UserService;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Button
+
+        var service = new UserService();
+        // we need to pass this service into the AddUserCommand
+        var command = new AddUserCommand(service);
+        // Pass this object into the button
+        var button = new Button(command);
+        // perform click, which goes back to the user service and adds the user.
+        button.click();
         // TextBox
         // Checkbox
-        //
     }
 }
